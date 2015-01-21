fa3b22fc99f780f1dd9fffc6656146906dcd7d7a
John Mealy <jmealy@advance.net>
2015-01-21 01:34:12 -0500

    Built self-propagation methods.

--- script/app.js
+++ script/app.js
@@ -1,30 +1,17 @@
 var $config = {
 	breakPoints: {
-		high: 10,
+		high: 9,
 		low: -1
 	},
-	directions: ['east','west,','north','south'],
 	neighbors: {
-		near: {
-			e: [1,0],			
-			w: [-1,0],			
-			n: [0,1],			
-			s: [0,-1],			
-			ne: [1,1],			
-			nw: [-1,1],			
-			se: [1,-1],			
-			sw: [-1,-1],			
-		},
-		far: {
-			e: [1,0],			
-			w: [-1,0],			
-			n: [0,1],			
-			s: [0,-1],			
-			ne: [1,1],			
-			nw: [-1,1],			
-			se: [1,-1],			
-			sw: [-1,-1],			
-		}
+		e: [1,0],			
+		w: [-1,0],			
+		n: [0,1],			
+		s: [0,-1],			
+		ne: [1,1],			
+		nw: [-1,1],			
+		se: [1,-1],			
+		sw: [-1,-1]		
 	},
 	dimensionTypes: ['x','y','z']
 };
@@ -33,130 +20,94 @@ var Cortex = function() {
 	"use strict";
 	var cortex = this;
 	cortex.grid = [];
-	cortex.Column = function(coords) {
+	cortex.Column = function($coords) {
 		var self = this;
 		var counter = 0;
+		$coords = $coords || {x:0,y:0,z:0};
 
 		// Get surrounding nodes:	
-		self.neighbors = (function makeNeighbors() {
+		var neighbors = function(coords) {
 			var i;
-			var a;
-			var mi;
-			var neighborArray = {};
-			var mutationInterval = (function () {
-				var y = 0;
-				var first = true;
-				return function() {
-					if(first) {
-						first = false;
-						return y;
-					}
-					first = true;
-					return y++;
-				};
-			})();
-			var oscillate = (function() {
-				var posneg = 1;
-				return function(){
-					posneg = posneg === 1 ? -1 : 1;
-					return posneg;
-				};
-			})();
-			var mutate = function(x) {
-				var newCoord = x + oscillate();
-				if(newCoord > $config.breakPoints.low && newCoord < $config.breakPoints.high) {
-					return newCoord;
-				} else {
-					return false;
-				}
-			};
-			for(i=0;i<$config.directions.length;i++) {
-				mi = mutationInterval();
-				neighborArray[$config.directions[i]] = {};
-				for(a=0;a<$config.dimensionTypes.length;a++) {
-					if(mi === a) {
-						neighborArray[$config.directions[i]][$config.dimensionTypes[a]] = mutate(coords[$config.dimensionTypes[a]]);
-						if(neighborArray[$config.directions[i]][$config.dimensionTypes[a]] === false) {
-							neighborArray[$config.directions[i]] = null;
-							break;
-						}
-					} else {
-						neighborArray[$config.directions[i]][$config.dimensionTypes[a]] = coords[$config.dimensionTypes[a]];
-					} 
-				}
-			}
-			return neighborArray;
-		})($config, coords);	
-		
-		// console.log(self.neighbors);
-		
-		self.propagation = function() {
-			var i;
-			var j = 0;
-			var iLength;
-			var jLength;
 			var x;
 			var y;
 			var z;
-			var positionArray = [];
-			var neighborhood = self.neighbors;
-			// var neighborhood = new self.neighbors;
-			for(i=0, iLength = $config.directions.length; i < iLength; i++) {
-				// for each neighbor...
-				
-				if(neighborhood[$config.directions[i]]) {				
-					x = neighborhood[$config.directions[i]][$config.dimensionTypes[0]];
-					y = neighborhood[$config.directions[i]][$config.dimensionTypes[1]];
-					z = neighborhood[$config.directions[i]][$config.dimensionTypes[2]];
-				                              
-				// for(j = 0, jLength = $config.dimensionTypes.length; j<jLength; j++) {
-					if(!window.Cortex.grid === undefined) {
-						window.Cortex.grid = [];
-					} 
-					if(window.Cortex.grid[x] === undefined) {
-						window.Cortex.grid[x] = [];
-					}
-					if(window.Cortex.grid[x][y] === undefined) {
-						window.Cortex.grid[x][y] = [];
-					}
-					if(window.Cortex.grid[x][y][z] === undefined) {
-						window.Cortex.grid[x][y][z] = {};
-						var stuff = new cortex.Column(neighborhood[$config.directions[i]]);
-						window.Cortex.grid[x][y][z] = stuff;
-					} else {
-						// window.Cortex.grid[x][y][z].checkin();
-					}
-				// }
-				
-					// window.Cortex.grid[neighborArray[$config.directions[i];
-				
-				
-					// for(i=0;i<$config.directions.length;i++) {
-						// x = self.neighbors[$config.directions[i]];
-						// console.log(curNeighbor());
-					// }
+			var iLength;
+			var neighborKeys = Object.keys($config.neighbors);
+			this.neighbors = {};
+			for(i=0, iLength = neighborKeys.length; i < iLength; i++) {
+				this.neighbors[neighborKeys[i]] = {};
+				x = coords.x + $config.neighbors[neighborKeys[i]][0];
+				if(x > $config.breakPoints.low && x < $config.breakPoints.high) {
+					this.neighbors[neighborKeys[i]].x = x;
+				} else {
+					this.neighbors[neighborKeys[i]] = null;
+					continue;
 				}
-			}			
-			
-			// 1. Create base
-			// 2. Detect base has been created by coords x and y respectively reaching 0 
-			// 3. Init the base
-			// 4. Repeat until detection that an init is called on a <4X and <4Y layer has been attempted
-			
-			
-			// if(
-				// window['Cortex'].cube[self.parentAddress.x][self.parentAddress.y][self.parentAddress.z] !== undefined &&
-				// self.parentAddress.z !== 0
-				// // && window['Cortex'].cube[coords.x][coords.y][coords.z + 1] !== undefined 
-				// // && window['Cortex'].cube[coords.x][coords.y + 1][coords.z] !== undefined 
-			// ) {
-				// newCoords = {}
-				// window['Cortex'].cube[self.parentAddress.x][self.parentAddress.y][self.parentAddress.z] = new cortex.Column(newCoords);
-			// }
-			// Create siblings
+				y = coords.y + $config.neighbors[neighborKeys[i]][1];
+				if(y > $config.breakPoints.low && y < $config.breakPoints.high) {
+					this.neighbors[neighborKeys[i]].y = y;
+				} else {
+					this.neighbors[neighborKeys[i]] = null;
+					continue;
+				}
+				
+				this.neighbors[neighborKeys[i]].z = coords.z;
+			}
+			this.parent = {
+				x: Math.round(coords.x/2),
+				y: Math.round(coords.y/2),
+				z: coords.z + 1
+			};
+			this.children = [];
+		};
+		self.neighborhood = new neighbors($coords);
+		self.registerChild = function(childCoords) {
+			var totalChildren = self.neighbors.children.push(childCoords);
+			if(totalChildren === 4){
+				cortex.grid[self.neighborhood.parent.x][self.neighborhood.parent.y][self.neighborhood.parent.z].propagate.up();
+			}
+		};
+		self.propagate = {
+			sideways : function () {
+				var i;
+				var j;
+				var neighborKeys = Object.keys(self.neighborhood.neighbors);
+				var totalNeighbors = neighborKeys.length;
+				var totalDimTypes = $config.dimensionTypes.length;
+				var neighborCoords = {};
+				var positionArray = [];
+				
+				// Deal with the peers:
+				for(i=0; i < totalNeighbors; i++) {
+					// for each neighbor address 
+					if(self.neighborhood.neighbors[neighborKeys[i]] !== null) {
+						for(j=0; j < totalDimTypes; j++) {
+							neighborCoords[neighborKeys[i]]= self.neighborhood.neighbors[neighborKeys[i]][$config.dimensionTypes[j]];
+						}
+						if(cortex.grid[neighborCoords.x] === undefined) {
+							cortex.grid[neighborCoords.x] = [];
+						}
+						if(cortex.grid[neighborCoords.x][neighborCoords.y] === undefined) {
+							cortex.grid[neighborCoords.x][neighborCoords.y] = [];
+						}
+						if(cortex.grid[neighborCoords.x][neighborCoords.y][neighborCoords.z] === undefined) {
+							cortex.grid[neighborCoords.x][neighborCoords.y][neighborCoords.z] = {};
+							cortex.grid[neighborCoords.x][neighborCoords.y][neighborCoords.z] = new cortex.Column(self.neighborhood[neighborKeys[i]]);
+							cortex.grid[neighborCoords.x][neighborCoords.y][neighborCoords.z].propagate.sideways();
+						}
+					}
+				}
+			},
+			// Deal with the parents
+			up: function() {
+				if(cortex.grid[self.neighborhood.parent.x][self.neighborhood.parent.y][self.neighborhood.parent.z] !== undefined) {
+					cortex.grid[self.neighborhood.parent.x][self.neighborhood.parent.y][self.neighborhood.parent.z].registerChild($coords);
+				} else {
+					cortex.grid[self.neighborhood.parent.x][self.neighborhood.parent.y][self.neighborhood.parent.z] = new cortex.Column(self.neighborhood.parent);
+				}
+			}
 		};
 		
-		self.propagation();
 		
 		
 		self.levels = [];
@@ -255,43 +206,9 @@ var Cortex = function() {
 	};
 	
 };
+
 window['Cortex'] = new Cortex();
+var protoColumn = new window['Cortex'].Column;
+protoColumn.propagate.sideways();
 
-var dims = {x:5,y:5,z:5};
 
-function buildCortex(dims) {
-	var cube = [];
-	// var x = 0;
-	// var y = 0;
-	// var z = 0;	
-	// for(x = 0;x<dims.x;x++) {
-		// cube[x] = [];	
-		// for(y = 0;y<dims.y;y++) {
-			// cube[x][y] = new window['Cortex'].Column({x:x,y:y});
-		// }
-	// }	
-	
-	// for(x = 0;x<dims.x;x++) {
-		// cube[x] = [];	
-		// for(y = 0;y<dims.y;y++) {
-			// cube[x][y] = [];
-			// for(z = 0;z<dims.z;z++) {
-				// // if() ratio between z and x/y 
-				// cube[x][y] = new window['Cortex'].Column({x:x,y:y});
-			// }
-		// }
-		// for(y = 0;y<dims.y;y++) {
-			// // for(z = 0;z<dims.z;z++) {
-				// window['Cortex'].cube[x][y].init();
-			// // }
-		// }
-		// if(window['Cortex'].cube[x][y][z]);
-	// }	
-
-	cube = new window['Cortex'].Column(dims);
-	console.log(cube);
-	return cube;
-}
-// var cube = buildCortex(dims);
-
-// window['Cortex'].propagate();
